2166b298e129c9fd32c035d8f39894046c6b68be
Björn Töpel <bjorn.topel@intel.com>
2018-10-10 13:08:52 +0200

    i40e: switch -> if/else to avoid spectre thunks
    23.3Mpps to 30.1Mpps
    
    Signed-off-by: Björn Töpel <bjorn.topel@intel.com>

--- drivers/net/ethernet/intel/i40e/i40e_xsk.c
+++ drivers/net/ethernet/intel/i40e/i40e_xsk.c
@@ -282,9 +282,9 @@ int i40e_xsk_umem_setup(struct i40e_vsi *vsi, struct xdp_umem *umem,
  **/
 static int i40e_run_xdp_zc(struct i40e_ring *rx_ring, struct xdp_buff *xdp)
 {
-	int err, result = I40E_XDP_PASS;
 	struct i40e_ring *xdp_ring;
 	struct bpf_prog *xdp_prog;
+	int err, result;
 	u32 act;
 
 	rcu_read_lock();
@@ -294,25 +294,21 @@ static int i40e_run_xdp_zc(struct i40e_ring *rx_ring, struct xdp_buff *xdp)
 	xdp_prog = READ_ONCE(rx_ring->xdp_prog);
 	act = bpf_prog_run_xdp(xdp_prog, xdp);
 	xdp->handle += xdp->data - xdp->data_hard_start;
-	switch (act) {
-	case XDP_PASS:
-		break;
-	case XDP_TX:
-		xdp_ring = rx_ring->vsi->xdp_rings[rx_ring->queue_index];
-		result = i40e_xmit_xdp_tx_ring(xdp, xdp_ring);
-		break;
-	case XDP_REDIRECT:
+	if (likely(act == XDP_REDIRECT)) {
 		err = xdp_do_redirect(rx_ring->netdev, xdp, xdp_prog);
 		result = !err ? I40E_XDP_REDIR : I40E_XDP_CONSUMED;
-		break;
-	default:
-		bpf_warn_invalid_xdp_action(act);
-	case XDP_ABORTED:
-		trace_xdp_exception(rx_ring->netdev, xdp_prog, act);
-		/* fallthrough -- handle aborts by dropping packet */
-	case XDP_DROP:
+	} else if(act == XDP_TX) {
+		xdp_ring = rx_ring->vsi->xdp_rings[rx_ring->queue_index];
+		result = i40e_xmit_xdp_tx_ring(xdp, xdp_ring);
+	} else if(act == XDP_PASS) {
+		result = I40E_XDP_PASS;
+	} else if(act == XDP_DROP) {
 		result = I40E_XDP_CONSUMED;
-		break;
+	} else {
+		if (act != XDP_ABORTED)
+			bpf_warn_invalid_xdp_action(act);
+		trace_xdp_exception(rx_ring->netdev, xdp_prog, act);
+		result = I40E_XDP_CONSUMED;
 	}
 	rcu_read_unlock();
 	return result;
