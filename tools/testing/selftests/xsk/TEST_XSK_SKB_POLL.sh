f3d4a7ae88ec5b054a39924db1687738c9b76fb1
Weqaar Janjua <weqaar.janjua@gmail.com>
2020-09-13 13:21:09 +0100

    selftests/xsk: xsk selftests - SKB POLL, NOPOLL
    Adds following tests:
    
    2. AF_XDP SKB mode
       Generic mode XDP that is driver independent, used when the driver does
       not have support for XDP. Works on any netdevice using sockets and
       generic XDP path. XDP hook from netif_receive_skb().
       a. nopoll - soft-irq processing
       b. poll - busy-pollng, using poll() syscall
          No longer waiting for device interrupts being generated/handled.
          Polling driver/device queues.
          For explanation on poll and no-poll refer to this RFC:
          https://patchwork.ozlabs.org/cover/1094083/
    
    Only copy-mode is supported.
    
    Total tests: 3.
    
    Signed-off-by: Weqaar Janjua <weqaar.a.janjua@intel.com>

---
+++ tools/testing/selftests/xsk/TEST_XSK_SKB_POLL.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-2.0
+# Copyright(c) 2020 Intel Corporation.
+
+#Includes
+. prereqs.sh
+
+XSKDIR=xdpprogs
+XSKOBJ=xdpxceiver
+SPECFILE=veth.spec
+
+if [ ! -f ${SPECFILE} ]; then
+    test_exit 5
+fi
+
+VETH0=$(cat ${SPECFILE} | cut -d':' -f 1)
+VETH1=$(cat ${SPECFILE} | cut -d':' -f 2 | cut -d',' -f 1)
+NS1=$(cat ${SPECFILE} | cut -d':' -f 2 | cut -d',' -f 2)
+
+vethXDPgeneric ${VETH0} ${VETH1} ${NS1}
+
+./${XSKDIR}/${XSKOBJ} -i ${VETH0} -i ${VETH1},${NS1} -S -p -C 10000
+
+retval=$?
+
+cleanup_exit ${VETH0} ${VETH1} ${NS1}
+
+test_exit $retval
---
+++ tools/testing/selftests/xsk/xdpprogs/xdpxceiver.h
@@ -0,0 +1,155 @@
+/* SPDX-License-Identifier: GPL-2.0
+ * Copyright(c) 2020 Intel Corporation.
+ */
+
+#ifndef XDPXCEIVER_H_
+#define XDPXCEIVER_H_
+
+#define MAX_SOCKS 1
+
+#ifndef SOL_XDP
+#define SOL_XDP 283
+#endif
+
+#ifndef AF_XDP
+#define AF_XDP 44
+#endif
+
+#ifndef PF_XDP
+#define PF_XDP AF_XDP
+#endif
+
+#define MIN_PKT_SIZE 64
+
+#define MAX_INTERFACES 2
+#define MAX_INTERFACE_NAME_CHARS 7
+#define MAX_INTERFACES_NAMESPACE_CHARS 10
+#define MAX_IP4_STR 16
+#define MAX_IP6_STR 40
+
+typedef __u64 u64;
+typedef __u32 u32;
+typedef __u16 u16;
+typedef __u8 u8;
+
+/*TESTS Specific*/
+enum TESTS {
+	ORDER_CONTENT_VALIDATE_XDP_SKB = 1,
+};
+u8 UUT;
+u32 NUM_FRAMES;
+
+static u32 opt_xdp_flags = XDP_FLAGS_UPDATE_IF_NOEXIST;
+static int opt_queue;
+static u32 opt_batch_size = 64;
+static int opt_pkt_count;
+static u16 opt_pkt_size = MIN_PKT_SIZE;
+static int opt_poll;
+static u32 opt_xdp_bind_flags = XDP_USE_NEED_WAKEUP;
+static u32 opt_umem_flags;
+static int opt_mmap_flags;
+static int opt_xsk_frame_size = XSK_UMEM__DEFAULT_FRAME_SIZE;
+static int opt_timeout = 1000;
+static bool opt_need_wakeup = true;
+static int num_socks = 1;
+static u8 pkt_data[XSK_UMEM__DEFAULT_FRAME_SIZE];
+static u32 pktCounter;
+static int sigVar;
+static u32 prevPkt = -1;
+
+struct xsk_umem_info {
+	struct xsk_ring_prod fq;
+	struct xsk_ring_cons cq;
+	struct xsk_umem *umem;
+	void *buffer;
+};
+
+struct xsk_socket_info {
+	struct xsk_ring_cons rx;
+	struct xsk_ring_prod tx;
+	struct xsk_umem_info *umem;
+	struct xsk_socket *xsk;
+	unsigned long rx_npkts;
+	unsigned long tx_npkts;
+	unsigned long prev_rx_npkts;
+	unsigned long prev_tx_npkts;
+	u32 outstanding_tx;
+};
+
+struct flow_vector {
+	enum fvector {
+		tx,
+		rx,
+		bidi,
+		undef,
+	} vector;
+};
+
+struct generic_data {
+	u32 seqnum;
+};
+
+struct ifaceConfigObj {
+	u8 dst_mac[ETH_ALEN];
+	u8 src_mac[ETH_ALEN];
+	struct in_addr dst_ip;
+	struct in_addr src_ip;
+	u16 src_port;
+	u16 dst_port;
+} *ifaceConfig;
+
+struct ifObjectStruct {
+	int opt_ifindex;
+	int ifDict_index;
+	char opt_if[MAX_INTERFACE_NAME_CHARS];
+	char opt_ns[MAX_INTERFACES_NAMESPACE_CHARS];
+	struct flow_vector fv;
+	struct xsk_socket_info *xsk;
+	struct xsk_umem_info *umem;
+	u8 dst_mac[ETH_ALEN];
+	u8 src_mac[ETH_ALEN];
+	u32 dst_ip;
+	u32 src_ip;
+	u16 src_port;
+	u16 dst_port;
+};
+static struct ifObjectStruct *ifDict[MAX_INTERFACES];
+
+/*packet*/
+#define ETH_FCS_SIZE		4
+#define PKT_HDR_SIZE		(sizeof(struct ethhdr) + sizeof(struct iphdr) + \
+				sizeof(struct udphdr))
+#define PKT_SIZE		(opt_pkt_size - ETH_FCS_SIZE)
+#define IP_PKT_SIZE		(PKT_SIZE - sizeof(struct ethhdr))
+#define UDP_PKT_SIZE		(IP_PKT_SIZE - sizeof(struct iphdr))
+#define UDP_PKT_DATA_SIZE	(UDP_PKT_SIZE - sizeof(struct udphdr))
+
+/*threads*/
+#define TMOUT_SEC	(3)
+#define EOT		(-1)
+#define USLEEP_MAX	200000
+#define THREAD_STACK	60000000
+atomic_int spinningTx;
+atomic_int spinningRx;
+pthread_mutex_t syncMutex;
+pthread_mutex_t syncMutexTx;
+pthread_mutex_t syncMutexRx;
+pthread_cond_t signalRxCondition;
+pthread_cond_t signalTxCondition;
+pthread_t t0, t1, rxthread, nsthread;
+pthread_attr_t attr;
+
+struct targs {
+	bool retptr;
+	int idx;
+};
+
+TAILQ_HEAD(head_s, pkt) head = TAILQ_HEAD_INITIALIZER(head);
+struct head_s *headp;
+struct pkt {
+	char *pktFrame;
+
+	TAILQ_ENTRY(pkt) pktNodes;
+} *pktNodeRx, *pktNodeRxQ;
+
+#endif				/* XDPXCEIVER_H */
