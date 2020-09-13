adaaabeaa350846413f594d82c3e49dd43c35add
Weqaar Janjua <weqaar.janjua@gmail.com>
2020-09-13 13:21:08 +0100

    selftests/xsk: xsk selftests framework
    This patch adds XSK Selftests framework under tools/testing/selftests/xsk.
    
    The following tests are run:
    
    1. PREREQUISITES
       Set up veth interfaces as per the topology shown in README:
       * setup two veth interfaces and one namespace
       ** veth<xxxx> in root namespace
       ** veth<yyyy> in af_xdp<xxxx> namespace
       ** namespace af_xdp<xxxx>
       * create a spec file veth.spec that includes this run-time configuration
         that is read by test scripts - filenames prefixed with TEST_XSK
       *** xxxx and yyyy are randomnly generated 4 digit numbers used to avoid
           conflict with any existing interface
    
    Total tests: 1.
    
    Signed-off-by: Weqaar Janjua <weqaar.a.janjua@intel.com>

---
+++ tools/testing/selftests/xsk/TEST_XSK.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# SPDX-License-Identifier: GPL-2.0
+# Copyright(c) 2020 Intel Corporation.
+
+#Includes
+. prereqs.sh
+
+XSKDIR=xdpprogs
+XSKOBJ=xdpxceiver
+SPECFILE=veth.spec
+
+if [ ! -f ${SPECFILE} ]; then
+    test_exit 5
+fi
+
+VETH0=$(cat ${SPECFILE} | cut -d':' -f 1)
+VETH1=$(cat ${SPECFILE} | cut -d':' -f 2 | cut -d',' -f 1)
+NS1=$(cat ${SPECFILE} | cut -d':' -f 2 | cut -d',' -f 2)
+
+retval=$?
+
+cleanup_exit ${VETH0} ${VETH1} ${NS1}
+
+test_exit $retval
